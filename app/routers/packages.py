fffdbbf39c5e7a084e45f097cdc6f7bca63c0284
owcen <czypionkaanna@gmail.com>
2024-02-06 19:11:00 +0100

    updated packages in routers

--- app/routers/packages.py
+++ app/routers/packages.py
@@ -10,3 +10,11 @@ router = APIRouter()
 async def get_all_packages():
     packages = list_serial(package_manager.find())
     return packages
+
+@router.post("/")
+async def create_package(package: Package):
+    package_manager.insert_one(dict(package))
+
+@router.put("/{id}")
+async def update_package(id: str, package: Package):
+    package_manager.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(package)})
