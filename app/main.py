e51956e85fa875af251276181829770ffcf6e150
DorotaSzota <dorotamalgorzata.szota@gmail.com>
2024-02-06 19:15:10 +0100

    main main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from app.routers import elf_route
+from app.routers import elf_route, packages
 
 app = FastAPI()
 
 app.include_router(elf_route.router)
-
+app.include_router(packages.router)
 async def root():
     return {"message": "Alive and kicking!"}
