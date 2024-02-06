3df3c2118be1d9ea46ffdbd289f350f245cfeff4
owcen <czypionkaanna@gmail.com>
2024-02-06 17:41:19 +0100

    Update user authentication logic to use bcrypt hashing algorithm

--- app/schema/schemas.py
+++ app/schema/schemas.py
@@ -1,9 +1,19 @@
 def individual_serial(elf) -> dict:
     return {
         "id": elf.id,
-        "name": elf.name,
+        "name": elf.elf_name,
         "is_on_holidays": elf.is_on_holidays
     }
 
 def list_serial(elves) -> list:
     return [individual_serial(elf) for elf in elves]
+
+def individual_serial(package) -> dict:
+    return {
+        "id": package.id,
+        "name": package.package_name,
+        "description": package.package_description,
+    }
+
+def list_serial(packages) -> list:
+    return [individual_serial(package) for package in packages]
