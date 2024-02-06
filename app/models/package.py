502cf9845e6f262b45ccfaddf0b27e56236873fd
DorotaSzota <dorotamalgorzata.szota@gmail.com>
2024-02-06 18:20:50 +0100

    class updates

--- app/models/package.py
+++ app/models/package.py
@@ -4,3 +4,8 @@ class Package(BaseModel):
     id: int
     package_name: str
     package_description: str
+
+    def __init__(self, id, package_name, package_description):
+        self.id = id
+        self.package_name = package_name
+        self.package_description = package_description
