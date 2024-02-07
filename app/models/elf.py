f60da5d17deb0f6bf5fc1b6672289ae79ba540e1
owcen <czypionkaanna@gmail.com>
2024-02-07 17:43:47 +0100

    deleted ids

--- app/models/elf.py
+++ app/models/elf.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 
 class Elf(BaseModel):
-    id: int
     elf_name: str
     is_on_holidays: bool
--- app/models/package.py
+++ app/models/package.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 
 class Package(BaseModel):
-    id: int
     package_name: str
     package_description: str
