7975172595da721bbe0242313feaf56fe20fd63b
owcen <czypionkaanna@gmail.com>
2024-02-07 17:38:31 +0100

    deleted init

--- app/models/package_manager.py
+++ app/models/package_manager.py
@@ -8,9 +8,3 @@ class PackageManager(BaseModel):
     package_id: Package.id
     elf_id: Elf.id
     package_status: Status
-
-    def __init__(self, id, package_id, elf_id, package_status):
-        self.id = id
-        self.package_id = package_id
-        self.elf_id = elf_id
-        self.package_status = package_status
